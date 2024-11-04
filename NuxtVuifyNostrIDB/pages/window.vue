94b9d47aae4c1306120e22ab0331f22b0d887db7
dentropy <dentropy@protonmail.com>
2024-11-03 23:05:08 -0500

    Learned how to interface with Nostr window extension

---
+++ NuxtVuifyNostrIDB/pages/window.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isMounted: false,
+    }
+  },
+  mounted() {
+    if (!this.isMounted) {
+      // Your function that needs to run only once after page loading
+      // this.doSomething();
+      this.isMounted = true;
+      console.log(window.nostr)
+      console.log(window.nostr.getPublicKey())
+    }
+  },
+}
+</script>
