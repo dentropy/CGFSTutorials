3d80508980820177d9e0fd62cc51081e6b666675
Paul Mullins <paulmullins@protonmail.com>
2024-12-31 15:36:41 -0500

    Added help command

--- NostrTutorial/lib/llmStuff/LLMSlashCommandParser.js
+++ NostrTutorial/lib/llmStuff/LLMSlashCommandParser.js
@@ -3,14 +3,14 @@ import parse from "jsr:@inro/slash-command-parser";
 export function LLMSlashCommandParser(message_content) {
   let parsed_content = {};
   try {
-    pased_content = parse(message_content);
+    parsed_content = parse(message_content);
   } catch (error) {
     return "Error Processing Slash Command: TODO";
   }
-  const help_response = "";
+  const default_help_response = "Please enter one of the following slash commands\nhelp\n/reset\n/llm to get more information on each of the commands for this bot";
   switch (parsed_content.command.toLocaleLowerCase()) {
     case "help":
-      break;
+      return default_help_response
     case "reset":
       break;
     case "llm":
