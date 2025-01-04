304e01d2e679dda5585cd354c6bee1470399bfbf
Paul Mullins <paulmullins@protonmail.com>
2025-01-04 02:26:30 -0500

    Removed some testing code

--- NostrTutorial/lib/llmStuff/LLMSlashCommandParser.js
+++ NostrTutorial/lib/llmStuff/LLMSlashCommandParser.js
@@ -142,24 +142,3 @@ export function LLMSlashCommandConvoParser(convo, models_supported) {
     model_selected: model_selected,
   };
 }
-
-function ExperimentalLLMSlashMessageParser(message_content) {
-  let parsed_content = {};
-  try {
-    parsed_content = parse(message_content);
-  } catch (error) {
-    return "Error Processing Slash Command: TODO";
-  }
-  const default_help_response =
-    "Please enter one of the following slash commands\nhelp\n/reset\n/llm to get more information on each of the commands for this bot";
-  switch (parsed_content.command.toLocaleLowerCase()) {
-    case "help":
-      return default_help_response;
-    case "reset":
-      break;
-    case "llm":
-      break;
-    default:
-      return help_response;
-  }
-}
