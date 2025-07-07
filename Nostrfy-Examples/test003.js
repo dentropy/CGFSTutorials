b4d63fc6d73a4a852cf12fa24d3fcfb142c4d088
Paul Mullins <paulmullins@protonmail.com>
2025-07-07 17:54:28 -0400

    Fixed the seed problem

--- Nostrfy-Examples/test003.js
+++ Nostrfy-Examples/test003.js
@@ -1,6 +1,6 @@
 // DOES NOT WORK, NPhraseSigner is Missing
 
-import { NPhraseSigner } from '@nostrify/nostrify';
+import { NPhraseSigner } from '@nostrify/seed';
 
 const signer = new NPhraseSigner('soap vault ahead turkey runway erosion february snow modify copy nephew rude', {
   account: 0, // Optional account number. Default is 0.
@@ -8,6 +8,9 @@ const signer = new NPhraseSigner('soap vault ahead turkey runway erosion februar
 });
 
 const pubkey = await signer.getPublicKey();
-const event = await signer.signEvent({ content: 'Hello, world!', kind: 1 })
+
+console.log(`\n\nPubkey is ${pubkey}\n\n`)
+
+const event = await signer.signEvent({ content: 'Hello, world!', kind: 1, tags: [], created_at: 0 })
 
 console.log(event)
